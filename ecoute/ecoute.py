dbd7167acec6a661e9e86d65955ff0e310cec1e4
Julien Duponchelle <julien@duponchelle.info>
2023-07-09 21:28:55 +0200

    Basic slicing

--- ecoute/ecoute.py
+++ ecoute/ecoute.py
@@ -1,5 +1,6 @@
 import openai
-
+from pydub import AudioSegment
+from tempfile import NamedTemporaryFile
 
 def transcribe(model, audio_file, response_format="json", **kwargs):
     """
@@ -12,4 +13,42 @@ def transcribe(model, audio_file, response_format="json", **kwargs):
     :param response_format: The format of the transcript output, in one of these options: json, text, srt, verbose_json, or vtt. (default json)
     :param language: The language of the input audio. Supplying the input language in ISO-639-1 format will improve accuracy and latency.
     """
-    return openai.Audio.transcribe(model, audio_file, response_format=response_format, **kwargs)
+    audio = AudioSegment.from_file(audio_file)
+
+    chunk_duration = 10 * 60
+
+    if audio.duration_seconds > chunk_duration:
+        result = None
+        for slice in _slice(model, audio, chunk_duration, **kwargs):
+            if slice is None:
+                break
+            if result is None:
+                result = slice
+            else:
+                result = _merge(result, slice)
+        return _export(result, response_format)
+    else:
+        return openai.Audio.transcribe(model, audio_file, response_format=response_format, **kwargs)
+
+
+def _export(transcript, response_format):
+    if response_format is None or response_format == "json":
+        return
+    elif response_format == "text":
+        return transcript.text
+    else:
+        raise NotImplementedError(
+            f"{response_format} is not a supported format")
+
+
+def _slice(model, audio, chunk_duration, **kwargs):
+    for slice in audio[::chunk_duration * 1000]:
+        with NamedTemporaryFile() as export:
+            slice.export(export)
+            export.seek(0)
+            yield openai.Audio.transcribe(model, slice, **kwargs)
+
+
+def _merge(o1, o2):
+    o1.text += " " + o2.text
+    return o1
--- tests/test_ecoute.py
+++ tests/test_ecoute.py
@@ -21,6 +21,17 @@ def empty_audio(tmp_path, scope="module"):
     return tmp_path / "empty.wav"
 
 
+@pytest.fixture
+def long_empty_audio(tmp_path, scope="module"):
+    """
+    An audio file of 11 minutes
+    """
+    with open(tmp_path / "long_empty.wav", "wb+") as f:
+        empty_audio = pydub.AudioSegment.silent(11 * 60 * 1000)
+        empty_audio.export(f, format="wav")
+    return tmp_path / "long_empty.wav"
+
+
 def test_transcribe_json(mocker, empty_audio):
     result = OpenAIObject(response_ms=100)
     result.text = "Hello World"
@@ -45,3 +56,16 @@ def test_transcribe_text(mocker, empty_audio):
     openai.Audio.transcribe.assert_called_with(
         "whisper-1", empty_audio, response_format="text")
     assert transcribe == "Hello World"
+
+
+def test_transcribe_text_long(mocker, long_empty_audio):
+    o1 = OpenAIObject(response_ms=100)
+    o1.text = "Hello World"
+    o2 = OpenAIObject(response_ms=200)
+    o2.text = "Bonjour le monde"
+    mocker.patch("openai.Audio.transcribe", side_effect=[o1, o2])
+
+    transcribe = ecoute.transcribe(
+        "whisper-1", long_empty_audio, response_format="text")
+
+    assert transcribe == "Hello World Bonjour le monde"
